67eed97bc5b5488539fd67c9461b5947d4d24ae4
Philemon Enkuselassie Hailemariam <philemonenkuselassie.hailemariam@duke.edu>
2022-10-15 03:11:08 -0400

    Updated wholdfoods webscraper code

--- wholefoods.py
+++ wholefoods.py
@@ -1,31 +1,35 @@
-from bs4 import BeautifulSoup as bs
-import requests 
+import time
+from selenium import webdriver
+from selenium.webdriver.common.by import By
 import pandas as pd
 
 # Get Wholefoods Website
-page = requests.get('https://www.wholefoodsmarket.com/sales-flyer?store-id=10041').text 
-soup = bs(page,'html.parser')
+chromeDriver = webdriver.Chrome("C://Users/meske/Documents/chromedriver_2.exe")
+time.sleep(3)
+chromeDriver.get('https://www.wholefoodsmarket.com/products/all-products')
+time.sleep(4)
+
+# Enter zip code 
+chromeDriver.find_element("xpath","//input[@id='pie-store-finder-modal-search-field']").send_keys(27708)
+time.sleep(3)
+chromeDriver.find_element("xpath","//li[@class='wfm-search-bar--list_item']").click()
+
+#Load more
+time.sleep(3)
+chromeDriver.find_element("xpath","//button[@class='w-button w-button--secondary w-button--load-more']").click()
+time.sleep(3)
+
 
 # Extract Category, Name, and price of products
-category = [line.get_text() for line in soup.find_all('div',{"class":"w-sales-tile__brand"})]
-name = [line.get_text() for line in soup.find_all('h4',{"class":"w-sales-tile__product"})]
-price = [line.get_text() for line in soup.find_all('span',{"class":"w-sales-tile__regular-price--strikeout"})]
-
-# Remove foods with no price
-foods_without_price = {'Single-Serve Meals*','Cut Pineapple*','Supplements*'}
-name_copy = name.copy()
-for food in foods_without_price:
-    category_index = name_copy.index(food)
-    name.remove(food)
-    del category[category_index]
+category = [element.text for element in chromeDriver.find_elements(By.XPATH, "//span[@data-testid='product-tile-brand']")]
+name = [element.text for element in chromeDriver.find_elements(By.XPATH, "//h2[@data-testid='product-tile-name']")]
+price = [element.find_element(By.CLASS_NAME,"regular_price").text for element in chromeDriver.find_elements(By.XPATH, "//div[@data-testid='pricing-block']")]
 
 #Make a dataframe 
 df = pd.DataFrame()
 df['category'] = category
 df['name'] = name
 df['price'] = price
-#Remove rows with no category
-df = df[df["category"]!=""]
 
 #Save it as a csv file
-df.to_csv("whole_foods_products.csv")
+df.to_csv("whole_foods_products(1).csv")
