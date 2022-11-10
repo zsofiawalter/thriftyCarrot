d12f542938b126dcd9243263fea83a4ceb59855a
Zsofia Walter <zsofia.walter8@gmail.com>
2022-11-10 10:02:14 -0500

    Add cart models

--- app/models/cartModel.py
+++ app/models/cartModel.py
@@ -1,53 +1,35 @@
 from flask import current_app as app
 
 
-# __uid__, __pid__, quantity
+# __uid__, cart_name, time_started
+# name of current cart user is working on
 class CartModel:
-    def __init__(self, uid, pid, quantity):
+    def __init__(self, uid, cart_name, time_started):
         self.uid = uid
-        self.pid = pid
-        self.quantity = quantity
+        self.cart_name = cart_name
+        self.time_started = time_started
 
     @staticmethod
     def get(uid):
         rows = app.db.execute('''
-SELECT uid, pid, quantity
+SELECT uid, cart_name, time_started
 FROM Carts
 WHERE uid = :uid
 ''',
                               uid=uid)
         return [CartModel(*row) for row in rows]
 
-# users cart names
-class CartFormModel:
-    def __init__(self, uid, cartName):
+# __uid__, product_name
+# list of things user wants to buy
+class CartListModel:
+    def __init__(self, uid, product_name):
         self.uid = uid
-        self.cartname = cartName
+        self.carproduct_namename = product_name
 
-# users products in cart
-class ProductsinCartForm:
+# __uid__, __pid__, quantity
+# exact product list user will buy
+class CartContentsModel:
     def __init__(self, uid, pid, quantity):
         self.uid = uid
         self.pid = pid
         self.quantity = quantity
-
-    @staticmethod
-    def addProductToCart(uid, product, quantity):
-        try:
-            rows = app.db.execute("""
-INSERT INTO Users(email, password, firstname, lastname)
-VALUES(:email, :password, :firstname, :lastname)
-RETURNING id
-""",
-                                  email=email,
-                                  password=generate_password_hash(password),
-                                  firstname=firstname, lastname=lastname)
-            id = rows[0][0]
-            return CartForm.get(id)
-        except Exception as e:
-            # likely email already in use; better error checking and reporting needed;
-            # the following simply prints the error to the console:
-            print(str(e))
-            return None
-
-
