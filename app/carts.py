1d4dfb36aeef6b314ad1f3b680f499e3b29f2729
Zsofia Walter <zsofia.walter8@gmail.com>
2022-11-10 09:55:25 -0500

    Added new false data for new schema

--- app/carts.py
+++ app/carts.py
@@ -1,6 +1,6 @@
 from flask import render_template
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
+from wtforms import StringField, SubmitField, FieldList, FormField, Form
 from wtforms.validators import DataRequired
 
 from .models.cartModel import CartModel
@@ -8,17 +8,18 @@ from .models.cartModel import CartModel
 from flask import Blueprint
 bp = Blueprint('carts', __name__)
 
-class userEntry(FlaskForm):
-    uid = StringField('UserID', validators=[DataRequired()])
-    submit = SubmitField('View current cart')
+class newCartProducts(Form):
+    product = StringField('Product', validators=[DataRequired()])
+
+class newCartForm(FlaskForm):
+    cartname = StringField('Cart Name', validators=[DataRequired()])
+    products = FieldList(FormField(newCartProducts), min_entries=1)
+    submit = SubmitField('Next')
 
 @bp.route('/newcart', methods=['GET', 'POST'])
 def newcart():
-    form = userEntry()
-    if form.validate_on_submit():
-        cart = CartModel.get(form.uid.data)
-    else:
-        cart = None
+    form = newCartForm()
+    cart = None
 
     return render_template('newCart.html',
                             cart = cart,
@@ -36,4 +37,3 @@ def carts():
     return render_template('carts.html',
                             cart = cart,
                             form = form)
-
--- app/models/cartModel.py
+++ app/models/cartModel.py
@@ -1,8 +1,8 @@
 from flask import current_app as app
 
+
 # __uid__, __pid__, quantity
 class CartModel:
-    #TODO: add product info to cart
     def __init__(self, uid, pid, quantity):
         self.uid = uid
         self.pid = pid
@@ -17,3 +17,37 @@ WHERE uid = :uid
 ''',
                               uid=uid)
         return [CartModel(*row) for row in rows]
+
+# users cart names
+class CartFormModel:
+    def __init__(self, uid, cartName):
+        self.uid = uid
+        self.cartname = cartName
+
+# users products in cart
+class ProductsinCartForm:
+    def __init__(self, uid, pid, quantity):
+        self.uid = uid
+        self.pid = pid
+        self.quantity = quantity
+
+    @staticmethod
+    def addProductToCart(uid, product, quantity):
+        try:
+            rows = app.db.execute("""
+INSERT INTO Users(email, password, firstname, lastname)
+VALUES(:email, :password, :firstname, :lastname)
+RETURNING id
+""",
+                                  email=email,
+                                  password=generate_password_hash(password),
+                                  firstname=firstname, lastname=lastname)
+            id = rows[0][0]
+            return CartForm.get(id)
+        except Exception as e:
+            # likely email already in use; better error checking and reporting needed;
+            # the following simply prints the error to the console:
+            print(str(e))
+            return None
+
+
