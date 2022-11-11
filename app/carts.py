611539aab010b1b11edb0f73e7ca430b98b07d1a
Zsofia Walter <zsofia.walter8@gmail.com>
2022-11-11 10:36:52 -0500

    Add page for specific new cart

--- app/carts.py
+++ app/carts.py
@@ -1,4 +1,4 @@
-from flask import render_template
+from flask import render_template, redirect, url_for
 from flask_login import current_user, login_required
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, FieldList, FormField, Form
@@ -29,15 +29,21 @@ def newcart():
 
     if form.validate_on_submit():
         uid = current_user.id
-        cart_name = form.cart_name
+        cart_name = form.cart_name.data
         time_started = datetime.now()
-        CartModel.startCart(uid, cart_name.data, time_started)
+        CartModel.startCart(uid, cart_name, time_started)
         # add product names to cart list
         for product in form.products.data:
             # add product to cart list
             CartListModel.addToCartList(uid, product['product'])
-
+        return redirect(url_for('carts.cartInProgress'))
 
     return render_template('newCart.html',
                             form = form,
                             _template = template_form)
+
+@bp.route('/<cart_name>', methods=['GET', 'POST'])
+@login_required
+def cartInProgress(cart_name):
+    return render_template('cartInProgress.html',
+                            cart_name = cart_name)
