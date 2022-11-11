0c468f981e164166a696b342aec046c95675070d
TerenceLee724 <115687930+TerenceLee724@users.noreply.github.com>
2022-11-11 12:52:45 -0500

    Add Old Carts to Home Page (#16)
    * Changed button in home.html
    
    * Ms3 (#1)
    
    * Commit for MS3
    
    * Commit for MS3
    
    Co-authored-by: Terence Lee <cl444@duke.edu>
    
    * New static method for oldCartContentModel.py
    
    Co-authored-by: Terence Lee <cl444@duke.edu>

--- app/home.py
+++ app/home.py
@@ -4,10 +4,18 @@ import datetime
 
 from .models.productModel import ProductModel
 from .models.oldCartModel import OldCartModel
+from .models.oldCartContentModel import OldCartContentModel
+
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
 
 from flask import Blueprint
 bp = Blueprint('home', __name__)
 
+class userEntry(FlaskForm):
+    uid = StringField('UserID', validators=[DataRequired()])
+    submit = SubmitField('Search For Carts')
 
 @bp.route('/home', methods=['GET', 'POST'])
 def home():
@@ -18,8 +26,17 @@ def home():
         products = ProductModel.get_all(datetime.datetime(2022, 10, 1, 0, 0, 0))
         purchases = OldCartModel.get_all_by_uid_since(
              current_user.id, datetime.datetime(1980, 9, 14, 0, 0, 0))
+        
+        carts = OldCartModel.get_recent_three_by_uid(current_user.id)
+        for cart in carts:
+            cart.time_created = cart.time_created.strftime("%b %d, %Y")
+        cartContent = OldCartContentModel.get_content_of_recent_three_by_uid(current_user.id)
+
         return render_template('home.html',
+                current_user=current_user,
                 avail_products=products,
-                purchase_history=purchases)
+                purchase_history=purchases,
+                carts=carts,
+                cartContent=cartContent)
     else:
         return redirect(url_for('users.login'))
