229959bb8efd3fd626e406af0b05d2b03a4ff06d
Philemon Hailemariam <ph131@duke.edu>
2022-12-05 02:34:45 -0500

    added all the filters to products page

--- app/products.py
+++ app/products.py
@@ -1,8 +1,8 @@
 from flask import render_template
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, IntegerField
+from wtforms import StringField, SubmitField, IntegerField, SelectField
 from wtforms.validators import DataRequired
-
+import datetime
 from .models.productModel import ProductModel
 
 from flask import Blueprint
@@ -26,31 +26,34 @@ def products():
                             products=products)
 
 
-class nameEntry(FlaskForm):
+class featureEntry(FlaskForm):
     name = StringField('Product name', validators=[DataRequired()])
-    minprice = IntegerField('Minimum Product price')
-    maxprice = IntegerField('Maximum Product price')
-    submit = SubmitField('Search')
-
-class priceEntry(FlaskForm):
-    k = StringField('Product name', validators=[DataRequired()])
+    minprice = IntegerField('Minimum price')
+    maxprice = IntegerField('Maximum price')
+    category = SelectField('Category', coerce=str)
+    store = SelectField('Store',coerce=str)
     submit = SubmitField('Search')
 
 @bp.route('/browse-products', methods=['GET', 'POST'])
 def browseproducts():
-    nameform = nameEntry()
-    priceform = priceEntry()
-    if nameform.validate_on_submit():
-        print("working")
-        products = ProductModel.get_by_feature(nameform.name.data,nameform.minprice.data, nameform.maxprice.data)
-    else:
-        print(1)    
-        print(nameform.maxprice.data)
-        products = None
+    featureform = featureEntry()
     allproducts = ProductModel.get_all()
+    categorychoices = []
+    storechoices= []
+    for i in allproducts:
+        if i.category not in categorychoices:
+            categorychoices.append(i.category)
+        if i.store not in storechoices:
+            storechoices.append(i.store)
+    featureform.category.choices = categorychoices
+    featureform.store.choices = storechoices
+    if featureform.validate_on_submit():
+        products = ProductModel.get_by_feature(featureform.name.data, featureform.minprice.data, featureform.maxprice.data,featureform.category.data, featureform.store.data)
+    else:
+        products = None
+    
     # render the page by adding information to the oldCarts.html file
     return render_template('browse-products.html',
-                            nameform=nameform,
-                            priceform=priceform,
+                            featureform=featureform,
                             products=products,
                             allproducts=allproducts)
