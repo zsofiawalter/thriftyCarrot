0e3d9d14659bb1d1e50f16429c511b713e258bae
Philenku <89503015+Philenku@users.noreply.github.com>
2022-12-07 20:18:40 -0500

    Productstab (#18)
    * Added carts endpoint
    
    * Added name and price filter for products page
    
    * added all the filters to products page

--- app/models/productModel.py
+++ app/models/productModel.py
@@ -22,6 +22,28 @@ WHERE id = :id
         return ProductModel(*(rows[0])) if rows is not None else None
 
     @staticmethod
+    def get_by_feature(name, minprice, maxprice,category,store):
+        rows = app.db.execute('''
+SELECT id, name, price, category, store, last_update
+FROM Products
+WHERE name LIKE :name and price >= :minprice and price <= :maxprice and category =:category and store = :store
+
+''',
+                                name='%'+name+'%',
+                                minprice=minprice,
+                                maxprice=maxprice,
+                                category=category,
+                                store=store)
+        return [ProductModel(*row) for row in rows]
+    @staticmethod 
+    def get_all_categories(since=datetime.datetime(2022, 10, 1, 0, 0, 0)):
+        rows = app.db.execute('''
+SELECT DISTINCT category
+FROM Products
+WHERE last_update >= :since
+''',
+                                since=since)
+        return [ProductModel(*row) for row in rows]
     def search_by_name(name):
         rows = app.db.execute('''
 SELECT id, name, price, category, store, last_update
